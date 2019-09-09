eaf149c049d88c38a6c4caa4f2b74724c2689bcb
yaame <969519545@qq.com>
2019-09-09 08:30:34 +0000

    futures 18

--- ln-manager/src/lib.rs
+++ ln-manager/src/lib.rs
@@ -17,7 +17,7 @@ extern crate tokio;
 extern crate tokio_codec;
 extern crate tokio_fs;
 extern crate tokio_io;
-extern crate tokio_timer;
+extern crate tokio_net;
 extern crate futures_timer;
 
 #[macro_use]
